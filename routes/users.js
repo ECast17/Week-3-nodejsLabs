e6f3fbc596c3832cb97da03130d3565628b9d75f
ECast17 <144755147+ECast17@users.noreply.github.com>
2024-03-14 17:22:28 -0400

    Update users.js
    more typo fixes PUT

--- routes/users.js
+++ routes/users.js
@@ -71,7 +71,7 @@ router.put("/:email", (req, res) => {
         }
         users = users.filter((user) => user.email != email);
         users.push(filtered_user);
-  res.send('User with the email ${email} updated.');
+  res.send('User with the email ' + ${email} + ' updated.');
     }
     else{
         res.send("Unable to find user!");
